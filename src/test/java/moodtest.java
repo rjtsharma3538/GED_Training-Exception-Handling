2b030bf499a6d970f4d59d1ae99612ef35a44a32
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2024-01-03 19:16:10 +0530

    commit

--- src/test/java/moodtest.java
+++ src/test/java/moodtest.java
@@ -3,10 +3,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class moodtest {
+
+    //TC - 1.1
     @Test
     public void moodAnalyzerTest1() {
         String msg = "I am in Sad Mood";
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         assertEquals("sad", moodAnalyzer.analyeMood(msg));
+    }
+
+    // Tc - 1.2
+    @Test
+    public void moodAnalyzerTest2(){
+        String msg = "I am in any mood";
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        assertEquals("happy", moodAnalyzer.analyeMood(msg));
     }
 }
